ba6e685afa9f0cced06eb3c203ef8a44be336e20
Nachiketa Gargi <nachiketa.gargi@gmail.com>
2018-10-24 18:05:54 -0400

    Threading start

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -14,61 +14,6 @@
 //==============================================================================
 MainComponent::MainComponent() {
   setSize(300, 300);
-  // Ideally, MonoSamples are only used when loading files and performing analysis. They
-  // produce an Analysis which is a compressed combination of FFT and Hilbert
-  // data. This data is then used for comparison instead of continuing to save the entire
-  // MonoSample to save memory.
-  const std::string sampleLibraryLocation =
-      "/home/nachi/code/JUCESampleFinder/test/";  // For testing
-  std::vector<std::shared_ptr<Analysis>> analyses;
-  DirectoryIterator iter(File(sampleLibraryLocation), true, "*.wav,*.aif,*.mp3");
-  int i = 0;
-  while (iter.next()) {
-    std::cout << iter.getEstimatedProgress() << "\r";
-    File currFile(iter.getFile());
-    if (!currFile.existsAsFile()) continue;
-    File analysisFile(currFile.getFullPathName() + ".saf");
-    std::shared_ptr<Analysis> a;
-    if (analysisFile.existsAsFile()) {
-      // read analysis from file
-      auto ifp = analysisFile.createInputStream();
-      if (ifp->failedToOpen()) {
-        // TODO actually have exceptions and stuff
-        std::cerr << "Couldn't open analysis file for reading" << std::endl;
-        continue;
-      }
-      a = Analysis::read(*ifp);
-
-      delete ifp;
-    } else {
-      auto s = new MonoSample(currFile);
-      a = s->computeAnalysis();
-      auto ofp = analysisFile.createOutputStream();
-      if (!ofp->openedOk()) {
-        continue;
-      }
-      Analysis::serialize(*ofp, *a);
-      delete ofp;
-      delete s;
-    }
-
-    analyses.push_back(a);
-    i++;
-  }
-  std::cout << "\n\n";
-
-  // std::ofstream out_file("test.out");  // For debugging in gnuplot
-  // std::copy(analyses[2]->analysisData.begin(), analyses[2]->analysisData.end(),
-  //           std::ostream_iterator<float>(out_file, "\n"));
-  int input;
-  std::cout << "enter the index of the one you want to search for: " << std::endl;
-  std::cin >> input;
-  auto testAnalysis = analyses[input];
-  std::cout << "Finding sample most simliar to " << testAnalysis->filename << std::endl;
-  Analysis::sortAnalyses(analyses, *testAnalysis, 10);
-  for (int i = 0; i < 10; i++) {
-    std::cout << "found " << analyses[i]->filename << " ranked " << i << std::endl;
-  }
 }
 
 MainComponent::~MainComponent() {}
